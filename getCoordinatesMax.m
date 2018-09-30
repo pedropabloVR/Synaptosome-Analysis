dc05dd065c43ea95ee2c0f38b4ee43f57fc9a132
Ezra Bruggeman <42326863+ezrabru@users.noreply.github.com>
2018-09-30 22:42:55 +0100

    added comments

--- getCoordinatesMax.m
+++ getCoordinatesMax.m
@@ -1,6 +1,15 @@
+% This function returns the x and y values at the location of the maximum
+% of a curve.
+% 
+% Author: Ezra Bruggeman, Laser Analytics Group
+% Last updated on 28 Sept 2018
+
 function [idx_x, idx_y] = getCoordinatesMax(X,Y)
 
+% Get y-value at location of the maximum of the curve
 idx_y = find(Y == max(Y(:)));
+
+% Get x-value at location of the maximum of the curve
 idx_x = X(idx_y);
 
 end
